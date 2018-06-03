f33fff3f86247a7ee8440e0ecae9942ba117745b
tree.bro <being.once@outlook.com>
2018-06-03 17:30:49 +0800

    页面逻辑调整

--- pages/calculator/calculator.js
+++ pages/calculator/calculator.js
@@ -5,11 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    StockID:'600000',
+    StockName:'',
+    MarketPrice:10.0,
+    ProfitPerShare:1.0,
+    ProfitSharingRatio:20.0,
+    TradeTax:1.0,
+    CompanyDuration:30,
+    DiscountRatio:7.0,
+    NaturalGrowthRate:0.5,
+    HighSpeedGrowthRate:5.0,
+    HighSpeedGrowthDuration:0,
+    ProfitSharingTax:0.5,
+    DepressionCycle:7,
+    DepressionLossRate:95.0,
+    StockHeldDuration:5
   },
 
   btnCalculateClicked: function(){
-    
+    //var marketPrice = 
   },
 
   /**
@@ -66,5 +80,68 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+  
+  setStockID:function(e){
+    //console.log(e);
+    this.setData({
+      StockID:e.detail.value
+    })
+  },
+
+  setStockName:function(e){
+    this.setData({
+      StockName:e.detail.value
+    })
+  },
+
+  setMarketPrice:function(e){
+    this.setData({
+      MarketPrice:e.detail.value
+    })
+  },
+
+  setProfitPerShare:function(e){
+    this.setData({
+      ProfitPerShare:e.detail.value
+    })
+  },
+
+  setProfitSharingRatio:function(e){
+    this.setData({
+      ProfitSharingRatio:e.detail.value
+    })
+  },
+
+  setTradeTax:function(e){
+    this.setData({
+      TradeTax:e.detail.value
+    })
+  },
+
+  setCompanyDuration:function(e){
+    this.setData({
+      CompanyDuration:e.detail.value
+    })
+  },
+
+  setDiscountRate:function(e){
+    this.setData({
+      DiscountRate:e.detail.value
+    })
+  },
+
+  setNaturalGrowthRate:function(e){
+    this.setData({
+      NaturalGrowthRate:e.detail.value
+    })
+  },
+
+  setHighSpeedGrowthRate:function(e){
+    this.setData({
+      HighSpeedGrowthRate:e.detail.value
+    })
+  },
+
+  
 })
